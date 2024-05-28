b44427231f3adb779e4a9f25827e9f5ec7f8afcd
Higor-Vinicius/API_VUE <higor.vinicius@fatec.sp.gov.br>
2024-05-27 23:25:47 -0300

    Finalizando projeto CRUD em go

--- controller/produtos.go
+++ controller/produtos.go
@@ -47,3 +47,50 @@ func Insert(w http.ResponseWriter, r *http.Request) {
 
 	http.Redirect(w, r, "/", 301)
 }
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	idProduto := r.URL.Query().Get("id")
+
+	models.DeletaProduto(idProduto)
+
+	http.Redirect(w, r, "/", 301)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	idProduto := r.URL.Query().Get("id")
+	dadosProd := models.BuscarProdutosId(idProduto)
+
+	temp.ExecuteTemplate(w, "Edit", dadosProd)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		nome := r.FormValue("nome")
+		descricao := r.FormValue("descricao")
+		quantidade := r.FormValue("quantidade")
+		preco := r.FormValue("preco")
+		IdProduto := r.FormValue("btnSalvar")
+
+		if IdProduto == "" {
+			panic("NÂO FOI POSSIVEL IDENTIFICAR O ID DO PRODUTO")
+		}
+
+		precoConvertido, err := strconv.ParseFloat(preco, 64)
+
+		if err != nil {
+			fmt.Println("ERRO AO CONVERTER VALOR ERRO: ", err)
+			os.Exit(1)
+		}
+
+		quantiConvertido, err := strconv.Atoi(quantidade)
+
+		if err != nil {
+			fmt.Println("ERRO AO CONVERTER QUANTIDADE ERRO: ", err)
+			os.Exit(1)
+		}
+
+		models.Update(nome, quantiConvertido, precoConvertido, descricao, IdProduto)
+	}
+
+	http.Redirect(w, r, "/", 301)
+}
--- models/produtos.go
+++ models/produtos.go
@@ -35,6 +35,7 @@ func BuscarProdutos() []Produto {
 			panic(err.Error())
 		}
 
+		p.Id = id
 		p.Nome = nome
 		p.Descricao = descricao
 		p.Preco = preco
@@ -64,5 +65,80 @@ func CriaNovoProduto(nome string, quantidade int, preco float64, descricao strin
 	}
 
 	fmt.Println("SUCESSO AO INSERIIR, LINHAS INSERIDA ")
+	defer dbConxao.Close()
+}
+
+func DeletaProduto(idProduto string) {
+	db := db.ConectaComBancoDeDados()
+
+	stm, err := db.Prepare("DELETE FROM PRODUTO WHERE ID = @p1")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer stm.Close()
+
+	_, erro := stm.Exec(sql.Named("p1", idProduto))
+
+	if erro != nil {
+		panic(erro.Error())
+	}
+
+	fmt.Println("SUCESSO AO EXCLUIR PRODUTO ID = ", idProduto)
+	defer db.Close()
+}
+
+func BuscarProdutosId(idProduto string) Produto {
+	db := db.ConectaComBancoDeDados()
+
+	stm, err := db.Query("SELECT * FROM PRODUTO WHERE ID = @p1", idProduto)
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	ProdutoBusca := Produto{}
+
+	for stm.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = stm.Scan(&id, &nome, &descricao, &preco, &quantidade)
+
+		if err != nil {
+			panic(err.Error())
+		}
+
+		ProdutoBusca.Id = id
+		ProdutoBusca.Nome = nome
+		ProdutoBusca.Descricao = descricao
+		ProdutoBusca.Preco = preco
+		ProdutoBusca.Quantidade = quantidade
+	}
+
+	defer db.Close()
+
+	return ProdutoBusca
+}
+
+func Update(nome string, quantidade int, preco float64, descricao string, idProduto string) {
+	dbConxao := db.ConectaComBancoDeDados()
+
+	stmt, err := dbConxao.Prepare("UPDATE PRODUTO SET NOME = @p1, DESCRICAO = @p2, PRECO =  @p3,QUANTIDADE = @p4 WHERE ID = @p5")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	defer stmt.Close()
+	_, erro := stmt.Exec(sql.Named("p1", nome), sql.Named("p2", descricao), sql.Named("p3", quantidade), sql.Named("p4", preco), sql.Named("p5", idProduto))
+
+	if erro != nil {
+		panic(erro.Error())
+	}
+
+	fmt.Println("DADOS ALTERADO COM SUCESSO")
 	defer dbConxao.Close()
 }
